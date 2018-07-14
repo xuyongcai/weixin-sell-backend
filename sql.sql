153c27216c834a323da85c92accddbb67787c790
xuyongcai <15089565436@163.com>
2018-07-14 16:55:24 +0800

    add description

---
+++ sql.sql
@@ -0,0 +1,124 @@
+/*
+SQLyog Enterprise v12.09 (64 bit)
+MySQL - 5.7.16 : Database - sell
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sell` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `sell`;
+
+/*Table structure for table `order_detail` */
+
+DROP TABLE IF EXISTS `order_detail`;
+
+CREATE TABLE `order_detail` (
+  `detail_id` varchar(32) NOT NULL,
+  `order_id` varchar(32) NOT NULL,
+  `product_id` varchar(32) NOT NULL,
+  `product_name` varchar(64) NOT NULL COMMENT '商品名称',
+  `product_price` decimal(8,2) NOT NULL COMMENT '当前价格,单位分',
+  `product_quantity` int(11) NOT NULL COMMENT '数量',
+  `product_icon` varchar(512) DEFAULT NULL COMMENT '小图',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`detail_id`),
+  KEY `idx_order_id` (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `order_detail` */
+
+insert  into `order_detail`(`detail_id`,`order_id`,`product_id`,`product_name`,`product_price`,`product_quantity`,`product_icon`,`create_time`,`update_time`) values ('12345671234567','1234567','234567','皮蛋粥','2.20',3,'http://xxxx.jpg','2018-07-11 19:27:55','2018-07-11 23:58:18'),('1531324576752582437','1234567','123456','皮蛋粥','3.20',1,'http://xxxxx.jpg','2018-07-11 23:56:16','2018-07-11 23:56:57'),('1531324576785304994','1531324576736207100','234567','皮皮虾','3.20',2,'http://xxxxx.jpg','2018-07-11 23:56:16','2018-07-11 23:56:16'),('1531324641573919666','1531324641568264963','123456','皮蛋粥','3.20',1,'http://xxxxx.jpg','2018-07-11 23:57:21','2018-07-11 23:57:21'),('1531324641601463955','1531324641568264963','234567','皮皮虾','3.20',2,'http://xxxxx.jpg','2018-07-11 23:57:21','2018-07-11 23:57:21');
+
+/*Table structure for table `order_master` */
+
+DROP TABLE IF EXISTS `order_master`;
+
+CREATE TABLE `order_master` (
+  `order_id` varchar(32) NOT NULL,
+  `buyer_name` varchar(32) NOT NULL COMMENT '买家名字',
+  `buyer_phone` varchar(32) NOT NULL COMMENT '买家电话',
+  `buyer_address` varchar(128) NOT NULL COMMENT '买家地址',
+  `buyer_openid` varchar(64) NOT NULL COMMENT '买家微信openid',
+  `order_amount` decimal(8,2) NOT NULL COMMENT '订单总金额',
+  `order_status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '订单状态, 默认为新下单',
+  `pay_status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '支付状态, 默认未支付',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`order_id`),
+  KEY `idx_buyer_openid` (`buyer_openid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `order_master` */
+
+insert  into `order_master`(`order_id`,`buyer_name`,`buyer_phone`,`buyer_address`,`buyer_openid`,`order_amount`,`order_status`,`pay_status`,`create_time`,`update_time`) values ('1234567','手撕代码','123456789123','天空','10086','2.50',1,1,'2018-07-11 19:35:39','2018-07-12 23:57:46'),('1531324576736207100','小饼干','123456789012','中原','10086','9.60',1,0,'2018-07-11 23:56:16','2018-07-13 00:16:33'),('1531324641568264963','小饼干','123456789012','中原','10086','9.60',0,0,'2018-07-11 23:57:21','2018-07-11 23:57:21');
+
+/*Table structure for table `product_category` */
+
+DROP TABLE IF EXISTS `product_category`;
+
+CREATE TABLE `product_category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(64) NOT NULL COMMENT '类目名字',
+  `category_type` int(11) NOT NULL COMMENT '类目编号',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `product_category` */
+
+insert  into `product_category`(`category_id`,`category_name`,`category_type`,`create_time`,`update_time`) values (1,'男装',2,'2018-07-10 23:19:59','2018-07-11 00:18:11'),(4,'专享',10,'2018-07-11 23:38:48','2018-07-14 14:56:56'),(5,'关心加班的你',101,'2018-07-12 18:34:53','2018-07-12 18:34:53'),(6,'凛冬已至',102,'2018-07-12 18:36:34','2018-07-12 18:36:35');
+
+/*Table structure for table `product_info` */
+
+DROP TABLE IF EXISTS `product_info`;
+
+CREATE TABLE `product_info` (
+  `product_id` varchar(32) NOT NULL,
+  `product_name` varchar(64) NOT NULL COMMENT '商品名称',
+  `product_price` decimal(8,2) NOT NULL COMMENT '单价',
+  `product_stock` int(11) NOT NULL COMMENT '库存',
+  `product_description` varchar(64) DEFAULT NULL COMMENT '描述',
+  `product_icon` varchar(512) DEFAULT NULL COMMENT '小图',
+  `product_status` tinyint(3) DEFAULT '0' COMMENT '商品状态,0正常1下架',
+  `category_type` int(11) NOT NULL COMMENT '类目编号',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `product_info` */
+
+insert  into `product_info`(`product_id`,`product_name`,`product_price`,`product_stock`,`product_description`,`product_icon`,`product_status`,`category_type`,`create_time`,`update_time`) values ('123456','皮蛋粥','3.20',102,'很好喝的粥','http://xxxxx.jpg',0,2,'2018-07-11 19:31:24','2018-07-11 23:28:07'),('1531405418395497972','烤肉','0.10',1000,'好吃不上火','',0,2,'2018-07-12 22:23:38','2018-07-12 22:23:38'),('234567','皮皮虾','3.20',102,'很好吃的虾','http://xxxxx.jpg',0,10,'2018-07-11 23:28:14','2018-07-14 14:22:04');
+
+/*Table structure for table `seller_info` */
+
+DROP TABLE IF EXISTS `seller_info`;
+
+CREATE TABLE `seller_info` (
+  `id` varchar(32) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `openid` varchar(64) NOT NULL COMMENT '微信openid',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='卖家信息表';
+
+/*Data for the table `seller_info` */
+
+insert  into `seller_info`(`id`,`username`,`password`,`openid`,`create_time`,`update_time`) values ('1531310567106328934','admin','admin','10010','2018-07-11 20:02:47','2018-07-11 20:02:47');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
